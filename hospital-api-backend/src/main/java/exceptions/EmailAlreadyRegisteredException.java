0b4f4928f28cc12b691e899f0011c65b59d1db88
rubens <rubenshds@gmail.com>
2025-04-15 19:30:55 -0300

    Manutenção da classe UserService, escrita de teste para a classe UserService com Mockito e JUnit

---
+++ hospital-api-backend/src/main/java/exceptions/EmailAlreadyRegisteredException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class EmailAlreadyRegisteredException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public EmailAlreadyRegisteredException(String msg) {
+		super(msg);
+	}
+
+	public EmailAlreadyRegisteredException(String msg, Throwable cause) {
+		super(msg,cause);
+	}
+}
--- hospital-api-backend/src/main/java/exceptions/handler/GlobalExceptionHandler.java
+++ hospital-api-backend/src/main/java/exceptions/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package exceptions.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(FirebaseAuthException.class)
 	public ResponseEntity<String> handleFirebaseException(FirebaseAuthException ex){
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao criar usuário!" + ex.getMessage());
+	}
+
+	@ExceptionHandler(UsernameNotFoundException.class)
+	public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado! " + ex.getMessage() );
 	}
 }
